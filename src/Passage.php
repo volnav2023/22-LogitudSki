a36345955ca5ca19fad3d6fe505b6bc5900560db
volnav2023 <github@flechet.com>
2020-10-19 10:43:29 +0200

    21 tests ok sur les 6 classes

--- src/Passage.php
+++ src/Passage.php
@@ -10,23 +10,37 @@ class Passage
         $nomParticipant,
         $prenomParticipant,
         $dateNaissParticipant,
-        $numero
+        $numPassage
     ) {
         if ($lieuEpreuve == '' or is_null($lieuEpreuve)
             or $dateEpreuve == '' or is_null($dateEpreuve)
             or $nomParticipant == '' or is_null($nomParticipant)
             or $prenomParticipant == '' or is_null($prenomParticipant)
             or $dateNaissParticipant == '' or is_null($dateNaissParticipant)
-            or is_null($numero)) {
+            or is_null($numPassage)) {
             throw new \Exception(
                 'Les données du passage sont incomplètes !'
             );
         };
 
-        if ($numero <> 1 and $numero <> 2) {
+        if ($numPassage <> 1 and $numPassage <> 2) {
             throw new \Exception(
                 'Le numéro de passage doit être 1 ou 2 !'
             );
         };
+    }
+
+    public function setTime($numPassage,$time)
+    {
+        if ($numPassage <> 1 and $numPassage <> 2) {
+            throw new \Exception(
+                'Le numéro de passage doit être 1 ou 2 !'
+            );
+        };
+        if ($time < 0) {
+            throw new \Exception(
+                'Le temps doit être positif !'
+            );
+        }
     }
 }
--- tests/PassageTest.php
+++ tests/PassageTest.php
@@ -2,68 +2,114 @@
 
 namespace Tests;
 
-use App\Inscription;
 use App\Passage;
 use DateTime;
 use Exception;
 
 it(
-    'Classe correctement instanciée',
+    'Construct : Classe correctement instanciée',
     function () {
         $lieuEpreuve = 'Vesoul';
         $dateEpreuve = new DateTime('2020-12-25');
         $nomParticipant = 'White';
         $prenomParticipant = 'Snow';
         $dateNaissParticipant = new DateTime('1962-11-01');
-        $numero = 1;
+        $numPassage = 1;
         $passage = new Passage(
             $lieuEpreuve,
             $dateEpreuve,
             $nomParticipant,
             $prenomParticipant,
             $dateNaissParticipant,
-            $numero
+            $numPassage
         );
         expect($passage)->toBeInstanceOf(Passage::class);
     }
 );
 
 it(
-    'Les données du passage sont incomplètes !',
+    'Construct : Les données du passage sont incomplètes !',
     function () {
         $lieuEpreuve = 'Vesoul';
         $dateEpreuve = new DateTime('2020-12-25');
+        echo date_format($dateEpreuve, 'd-m-Y H:i:s');
         $nomParticipant = 'White';
         $prenomParticipant = '';
         $dateNaissParticipant = new DateTime('1962-11-01');
-        $numero = 1;
+        echo date_format($dateNaissParticipant, 'd-m-Y H:i:s');
+        $numPassage = 1;
         $passage = new Passage(
             $lieuEpreuve,
             $dateEpreuve,
             $nomParticipant,
             $prenomParticipant,
             $dateNaissParticipant,
-            $numero
+            $numPassage
         );
     }
 )->throws(Exception::class, 'Les données du passage sont incomplètes !');
 
 it(
-    'Le numéro de passage doit être 1 ou 2 !',
+    'Construct : Le numéro de passage doit être 1 ou 2 !',
     function () {
         $lieuEpreuve = 'Vesoul';
         $dateEpreuve = new DateTime('2020-12-25');
         $nomParticipant = 'White';
         $prenomParticipant = 'Snow';
         $dateNaissParticipant = new DateTime('1962-11-01');
-        $numero = 3;
+        $numPassage = 3;
         $passage = new Passage(
             $lieuEpreuve,
             $dateEpreuve,
             $nomParticipant,
             $prenomParticipant,
             $dateNaissParticipant,
-            $numero
+            $numPassage
         );
     }
 )->throws(Exception::class, 'Le numéro de passage doit être 1 ou 2 !');
+
+it(
+    'SetTime : Le numéro de passage doit être 1 ou 2 !',
+    function () {
+        $lieuEpreuve = 'Vesoul';
+        $dateEpreuve = new DateTime('2020-12-25');
+        $nomParticipant = 'White';
+        $prenomParticipant = 'Snow';
+        $dateNaissParticipant = new DateTime('1962-11-01');
+        $numPassage = 3;
+        $passage = new Passage(
+            $lieuEpreuve,
+            $dateEpreuve,
+            $nomParticipant,
+            $prenomParticipant,
+            $dateNaissParticipant,
+            $numPassage
+        );
+        $time = 10 ** 9;
+        $passage->setTime1($numPassage, $time);
+    }
+)->throws(Exception::class, 'Le numéro de passage doit être 1 ou 2 !');
+
+it(
+    'SetTime : Le temps doit être positif !',
+    function () {
+        $lieuEpreuve = 'Vesoul';
+        $dateEpreuve = new DateTime('2020-12-25');
+        $nomParticipant = 'White';
+        $prenomParticipant = 'Snow';
+        $dateNaissParticipant = new DateTime('1962-11-01');
+        $numPassage = 2;
+        $passage = new Passage(
+            $lieuEpreuve,
+            $dateEpreuve,
+            $nomParticipant,
+            $prenomParticipant,
+            $dateNaissParticipant,
+            $numPassage
+        );
+        $time = -10 ** 9;
+        $numPassage = 1;
+        $passage->setTime($numPassage, $time);
+    }
+)->throws(Exception::class, 'Le temps doit être positif !');
