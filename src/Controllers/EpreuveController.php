eab708a6b70981e74afa23527ddd69cc9a6c7b6f
volnav2023 <github@flechet.com>
2020-10-20 16:58:50 +0200

    http foundation intégré et fonctionne et nettoyé !

--- src/Controllers/EpreuveController.php
+++ src/Controllers/EpreuveController.php
@@ -14,9 +14,9 @@ class EpreuveController
 
     public function participantsListe(Request $request)
     {
-        echo 'Ici EpreuveController->participantsListe';
-        dump('$request : ',$request);
-        dump('$request->query->get(\'lieu\') : ',$request->query->get('lieu'));
-        dump('$request->query->get(\'date\') : ',$request->query->get('date'));
+        echo 'EpreuveController->participantsListe affiche la liste des participants pour l\'épreuves :';
+        dump($request->query->get('lieu'));
+        dump($request->query->get('date'));
+//        dump('$request : ',$request);
     }
 }
